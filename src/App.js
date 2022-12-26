bffbe5b19b9c419c9857ba58f1ee3b53598f5936
Robarto <robartpetrov@gmail.com>
2022-12-26 20:03:38 +0200

    Finished mobile styles and logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./styles.scss";
 function App() {
+  const [isChecked, setIsChecked] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Our Pricing</h1>
+      <label htmlFor="checkbox">
+        <span>Annually</span>
+        <input
+          type="checkbox"
+          defaultChecked
+          onChange={() => setIsChecked((curr) => (curr = !curr))}
+          id="checkbox"
+        />
+        <span>Monthly</span>
+      </label>
+      <div className="card">
+        <h2>Basic</h2>
+        <ul>
+          <li className="price">
+            $<span>{isChecked ? 19.99 : 199.99}</span>
+          </li>
+          <li>500 GB Storage</li>
+          <li>2 Users Allowed</li>
+          <li>Send up to 3 GB</li>
+        </ul>
+        <button>Learn More</button>
+      </div>
+      <div className="card">
+        <h2>Professional</h2>
+        <ul>
+          <li className="price">
+            $<span>{isChecked ? 24.99 : 249.99}</span>
+          </li>
+          <li>1 TB Storage</li>
+          <li>5 Users Allowed</li>
+          <li>Send up to 10 GB</li>
+        </ul>
+        <button>Learn More</button>
+      </div>
+      <div className="card">
+        <h2>Master</h2>
+        <ul>
+          <li className="price">
+            $<span>{isChecked ? 39.99 : 399.99}</span>
+          </li>
+          <li>2 TB Storage</li>
+          <li>10 Users Allowed</li>
+          <li>Send up to 20 GB</li>
+        </ul>
+        <button>Learn More</button>
+      </div>
+    </main>
   );
 }
 
